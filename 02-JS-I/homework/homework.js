02acbd04c35d1e1fc54840ef4139e18463718af3
z-ion1988 <juanro1988.jr@gmail.com>
2022-08-23 22:17:10 -0500

    primera tarea

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,22 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "hola";
+const nuevaString = "puto";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 78;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 2 < 10;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 4;g
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,7 +28,9 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  var nombre = "juancho";
   
+  return nombre
 }
 
 function suma(x, y) {
@@ -46,6 +48,7 @@ function resta(x, y) {
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
+
   // Tu código:
   
 }
